78c1b98a25cb4da218d3657f7d6200f807445222
durandkwok-snowflake <109616231+durandkwok-snowflake@users.noreply.github.com>
2023-12-21 17:29:23 -0500

    Add files via upload

---
+++ dk_snowflake_scaling_compute_demo.sql
@@ -0,0 +1,29 @@
+use database snowflake_sample_data;
+use schema tpch_sf1000;
+use warehouse scaling_test;
+
+ALTER SESSION SET USE_CACHED_RESULT = FALSE;
+
+
+SELECT
+    p.p_partkey,
+    p.p_name,
+    p.p_brand,
+    p.p_type,
+    p.p_size,
+    SUM(CASE WHEN DATEDIFF(DAY,CURRENT_DATE, o.o_orderdate) <= 30 THEN l.l_quantity ELSE 0 END) AS last_30_days_qty,
+    SUM(CASE WHEN DATEDIFF(DAY, CURRENT_DATE, o.o_orderdate) > 30 AND DATEDIFF(DAY, CURRENT_DATE, o.o_orderdate) <= 60 THEN l.l_quantity ELSE 0 END) AS last_31_60_days_qty,
+   SUM(CASE WHEN DATEDIFF(DAY, CURRENT_DATE, o.o_orderdate) > 60 AND DATEDIFF(DAY, CURRENT_DATE, o.o_orderdate) <= 90 THEN l.l_quantity ELSE 0 END) AS last_61_90_days_qty,
+   SUM(CASE WHEN DATEDIFF(DAY, CURRENT_DATE, o.o_orderdate) > 90 THEN l.l_quantity ELSE 0 END) AS over_90_days_qty
+FROM
+    part p
+JOIN
+    lineitem l ON p.p_partkey = l.l_partkey
+JOIN
+    orders o ON l.l_orderkey = o.o_orderkey
+GROUP BY
+    p.p_partkey, p.p_name, p.p_brand, p.p_type, p.p_size
+ORDER BY
+    p.p_partkey
+;
+
